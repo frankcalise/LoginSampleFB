b841b170afae5ef5a11abed1614951503e111077
Frank Calise <fcalise@gmail.com>
2014-06-14 19:24:16 -0400

    Initial Commit

---
+++ LoginSampleFB/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoginSampleFB
+//
+//  Created by Frank Calise on 6/14/14.
+//  Copyright (c) 2014 Frankcalise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LoginSampleFBTests/LoginSampleFBTests.m
@@ -0,0 +1,34 @@
+//
+//  LoginSampleFBTests.m
+//  LoginSampleFBTests
+//
+//  Created by Frank Calise on 6/14/14.
+//  Copyright (c) 2014 Frankcalise. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoginSampleFBTests : XCTestCase
+
+@end
+
+@implementation LoginSampleFBTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
