aad889b64966094b7d3519fc91edd59ad1942ea9
Frank Calise <fcalise@gmail.com>
2014-06-14 21:58:39 -0400

    implemented FBLoginViewDelegate
    pulled profile pic, username and email from the user’s profile and
    displayed after login

--- LoginSampleFB/ViewController.m
+++ LoginSampleFB/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+- (void)toggleHiddenState:(BOOL)shouldHide;
+
 @end
 
 @implementation ViewController
@@ -18,12 +20,52 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self toggleHiddenState:YES];
+    self.lblLoginStatus.text = @"";
+    self.loginButton.readPermissions = @[@"public_profile", @"email"];
+    
+    self.loginButton.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)toggleHiddenState:(BOOL)shouldHide
+{
+    self.lblUsername.hidden = shouldHide;
+    self.lblEmail.hidden = shouldHide;
+    self.profilePicture.hidden = shouldHide;
+}
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView
+{
+    self.lblLoginStatus.text = @"You are logged in.";
+    
+    [self toggleHiddenState:NO];
+}
+
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user
+{
+    NSLog(@"%@", user);
+    self.profilePicture.profileID = user.objectID;
+    self.lblUsername.text = user.name;
+    self.lblEmail.text = [user objectForKey:@"email"];
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView
+{
+    self.lblLoginStatus.text = @"You are logged out";
+    
+    [self toggleHiddenState:YES];
+}
+
+- (void)loginView:(FBLoginView *)loginView handleError:(NSError *)error
+{
+    NSLog(@"%@", [error localizedDescription]);
 }
 
 @end
